eb47e25e6539ea185e5b309460b6ce7d26657067
isaicass28 <isaicaceresramirez@gmail.com>
2026-03-11 23:46:43 -0500

    22 commit

--- client/src/components/ui/Logo.jsx
+++ client/src/components/ui/Logo.jsx
@@ -1,10 +1,15 @@
 import styles from './Logo.module.css';
-import logo from '../../assets/logo-noteplace.svg';
+import { useTheme } from '../../hooks/useTheme';
+import logoDark from '../../assets/logo_dark.png';
+import logoLight from '../../assets/logo_light.png';
 
 function Logo() {
+  const { isDark } = useTheme();
+  const logoSrc = isDark ? logoLight : logoDark;
+
   return (
     <div className={styles.logo}>
-      <img src={logo} alt="NotePlace logo" />
+      <img className={styles.logoImage} src={logoSrc} alt="NotePlace" />
     </div>
   );
 }
